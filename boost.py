6ec4ad1f9876cf32c9457355ee7e25b5abb7e09d
Mathis Antony <sveitser@gmail.com>
2015-05-21 14:13:23 +0800

    optimize xgboost parameters a bit

--- boost.py
+++ boost.py
@@ -4,7 +4,7 @@ import os
 import click
 import numpy as np
 import pandas as pd
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, make_scorer
 from sklearn.grid_search import GridSearchCV
 from xgboost import XGBRegressor
 
@@ -44,32 +44,39 @@ def fit(cnf, predict):
                             transforms['test']])
         y_train = labels
     else:
-        X_train, X_test, y_train, y_test = util.split(X_train, labels)
+        #X_train, X_test, y_train, y_test = util.split(X_train, labels)
+        pass
 
     est = XGBRegressor(
         n_estimators=100, 
         #silent=0,
-        #subsample=0.5,
-        #colsample_bytree=0.5,
+        subsample=0.1,
+        colsample_bytree=0.5,
+        max_depth=1,
     )
-    grid = {
-        'subsample': [0.1, 0.5, 0.8, 1.0],
-        'colsample_bytree': [0.1, 0.5, 0.8, 1.0],
-        'max_depth': [1, 2, 3, 4, 5, 6],
-        'n_estimators': [50, 100, 200],
-    }
-    gs = GridSearchCV(est, grid, verbose=2)
-
-    gs.fit(X_train, y_train)
-    pd.set_option('display.height', 500)
-    pd.set_option('display.max_rows', 500)
-    df = util.grid_search_score_dataframe(gs)
-    print(df)
-    df.to_csv('grid_scores.csv')
-
-    y_pred = np.round(gs.predict(X_test)).astype(int)
+    if not predict:
+        grid = {
+            #'subsample': [0.01, 0.02, 0.05, 0.1, 0.15, 0.2],
+            #'colsample_bytree': [0.5, 1.0],
+            #'max_depth': [1],
+            'learning_rate': [0.01, 0.05, 0.1, 0.15],
+            'n_estimators': [50, 100, 200],
+        }
+        indices = util.split_indices(labels)
+        kappa_scorer = make_scorer(util.kappa)
+        gs = GridSearchCV(est, grid, verbose=3, cv=[indices], 
+                          scoring=kappa_scorer)
+        y_train = labels
+        gs.fit(X_train, y_train)
+        pd.set_option('display.height', 500)
+        pd.set_option('display.max_rows', 500)
+        df = util.grid_search_score_dataframe(gs)
+        print(df)
+        df.to_csv('grid_scores.csv')
+        est = gs.best_estimator_
 
     if predict:
+        y_pred = np.round(est.predict(X_test)).astype(int)
         submission_filename = util.get_submission_filename()
         files = util.get_image_files(model.get('test_dir', TEST_DIR))
         names = util.get_names(files)
@@ -78,9 +85,9 @@ def fit(cnf, predict):
         predictions = pd.concat([image_column, level_column], axis=1)
         predictions.to_csv(submission_filename, index=False)
         print("saved predictions to {}".format(submission_filename))
-    else:
-        print(confusion_matrix(y_test.astype(int), y_pred.astype(int)))
-        print("kappa {}".format(util.kappa(y_test, y_pred)))
+    #else:
+    #    print(confusion_matrix(y_test.astype(int), y_pred.astype(int)))
+    #    print("kappa {}".format(util.kappa(y_test, y_pred)))
 
 if __name__ == '__main__':
     fit()
