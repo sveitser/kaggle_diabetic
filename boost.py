8eaa59165f8ca639f0d77340a482d45957fa6305
Mathis Antony <sveitser@gmail.com>
2015-05-23 11:53:10 +0800

    per patient prediction

--- boost.py
+++ boost.py
@@ -25,7 +25,8 @@ TRANSFORM_DIR = 'data/transform_spatial'
               help="Path or name of configuration module.")
 @click.option('--predict', is_flag=True, default=False)
 @click.option('--grid_search', is_flag=True, default=False)
-def fit(cnf, predict, grid_search):
+@click.option('--per_patient', is_flag=True, default=False)
+def fit(cnf, predict, grid_search, per_patient):
 
     model = util.load_module(cnf).model
     files = util.get_image_files(model.get('train_dir', TRAIN_DIR))
@@ -67,6 +68,25 @@ def fit(cnf, predict, grid_search):
     #    ('fit', LinearSVR(verbose=3, max_iter=10000)),
     #])
 
+    if per_patient:
+        X_train = X_train.reshape(len(X_train) / 2, -1).repeat(2, axis=0)
+        if not predict:
+            a, b = util.split_indices(labels)
+            
+            even = b[b%2 == 0]
+            odd = b[b%2 != 0]
+            te = np.array(
+                list(set.union(set(even), set(even+1), set(odd), set(odd-1))),
+                dtype=int)
+            logical = np.zeros(len(labels), dtype=bool)
+            logical[te] = True
+            tr = np.where(~logical)[0]
+            X_train, X_test = X_train[tr], X_train[te]
+            y_train, y_test = labels[tr], labels[te]
+    else:
+        X_train, X_test, y_train, y_test = util.split(X_train, labels)
+
+
     if not predict:
         grid = {
             #'subsample': [0.01, 0.02, 0.05, 0.1, 0.15, 0.2],
@@ -94,23 +114,6 @@ def fit(cnf, predict, grid_search):
             df.to_csv('grid_scores.csv')
             est = gs.best_estimator_
         else:
-            X_train = X_train.reshape(len(X_train) / 2, -1).repeat(2, axis=0)
-            #labels = labels.reshape(len(labels) // 2, 2)
-            #X_train, X_test, y_train, y_test = util.split(X_train, labels)
-            a, b = util.split_indices(labels)
-            
-            even = b[b%2 == 0]
-            odd = b[b%2 != 0]
-            te = np.array(
-                list(set.union(set(even), set(even+1), set(odd), set(odd-1))),
-                dtype=int)
-            print(len(te))
-            logical = np.zeros(len(labels), dtype=bool)
-            logical[te] = True
-            tr = np.where(~logical)[0]
-            X_train, X_test = X_train[tr], X_train[te]
-            y_train, y_test = labels[tr], labels[te]
-            print(y_test.shape, y_train.shape)
             est.fit(X_train, y_train)
             y_pred = np.round(est.predict(X_test)).astype(int)
             #y_pred = y_pred.reshape(len(y_pred) * 2, 1)
