1cb4cc55e505826c4f78d564bd1c593ee26b36f3
Mathis Antony <sveitser@gmail.com>
2015-08-03 16:25:55 +0800

    remove unused files and functions

--- quadratic_weighted_kappa.py
+++ quadratic_weighted_kappa.py
@@ -1,10 +1,10 @@
-#
-#   Mathis: taken from here,
-#   https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/quadratic_weighted_kappa.py
-#   slightly modified to suit our needs.
-#
-import numpy as np
+"""Quadratic weighted kappa metric.
 
+   Mathis: taken from here,
+   https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/quadratic_weighted_kappa.py
+   slightly modified to suit our needs.
+"""
+import numpy as np
 
 def confusion_matrix(rater_a, rater_b, min_rating=None, max_rating=None):
     """
@@ -62,9 +62,6 @@ def quadratic_weighted_kappa(rater_a, rater_b, min_rating=0, max_rating=4):
     rater_a = np.clip(rater_a, min_rating, max_rating)
     rater_b = np.clip(rater_b, min_rating, max_rating)
 
-    #rater_a = np.array(rater_a)
-    #rater_b = np.array(rater_b)
-
     rater_a = np.round(rater_a).astype(int).ravel()
     rater_a[~np.isfinite(rater_a)] = 0
     rater_b = np.round(rater_b).astype(int).ravel()
@@ -90,155 +87,9 @@ def quadratic_weighted_kappa(rater_a, rater_b, min_rating=0, max_rating=4):
         for j in range(num_ratings):
             expected_count = (hist_rater_a[i] * hist_rater_b[j]
                               / num_scored_items)
-            #try:
-            #    d = pow(i - j, 2.0) / pow(num_ratings - 1, 2.0)
-            #except ZeroDivisionError:
-            #    return np.nan
             d = pow(i - j, 2.0) / pow(num_ratings - 1, 2.0)
             numerator += d * conf_mat[i][j] / num_scored_items
             denominator += d * expected_count / num_scored_items
 
     return 1.0 - numerator / denominator
 
-
-def linear_weighted_kappa(rater_a, rater_b, min_rating=None, max_rating=None):
-    """
-    Calculates the linear weighted kappa
-    linear_weighted_kappa calculates the linear weighted kappa
-    value, which is a measure of inter-rater agreement between two raters
-    that provide discrete numeric ratings.  Potential values range from -1
-    (representing complete disagreement) to 1 (representing complete
-    agreement).  A kappa value of 0 is expected if all agreement is due to
-    chance.
-
-    linear_weighted_kappa(rater_a, rater_b), where rater_a and rater_b
-    each correspond to a list of integer ratings.  These lists must have the
-    same length.
-
-    The ratings should be integers, and it is assumed that they contain
-    the complete range of possible ratings.
-
-    linear_weighted_kappa(X, min_rating, max_rating), where min_rating
-    is the minimum possible rating, and max_rating is the maximum possible
-    rating
-    """
-    assert(len(rater_a) == len(rater_b))
-    if min_rating is None:
-        min_rating = min(rater_a + rater_b)
-    if max_rating is None:
-        max_rating = max(rater_a + rater_b)
-    conf_mat = confusion_matrix(rater_a, rater_b,
-                                min_rating, max_rating)
-    num_ratings = len(conf_mat)
-    num_scored_items = float(len(rater_a))
-
-    hist_rater_a = histogram(rater_a, min_rating, max_rating)
-    hist_rater_b = histogram(rater_b, min_rating, max_rating)
-
-    numerator = 0.0
-    denominator = 0.0
-
-    for i in range(num_ratings):
-        for j in range(num_ratings):
-            expected_count = (hist_rater_a[i] * hist_rater_b[j]
-                              / num_scored_items)
-            d = abs(i - j) / float(num_ratings - 1)
-            numerator += d * conf_mat[i][j] / num_scored_items
-            denominator += d * expected_count / num_scored_items
-
-    return 1.0 - numerator / denominator
-
-
-def kappa(rater_a, rater_b, min_rating=None, max_rating=None):
-    """
-    Calculates the kappa
-    kappa calculates the kappa
-    value, which is a measure of inter-rater agreement between two raters
-    that provide discrete numeric ratings.  Potential values range from -1
-    (representing complete disagreement) to 1 (representing complete
-    agreement).  A kappa value of 0 is expected if all agreement is due to
-    chance.
-
-    kappa(rater_a, rater_b), where rater_a and rater_b
-    each correspond to a list of integer ratings.  These lists must have the
-    same length.
-
-    The ratings should be integers, and it is assumed that they contain
-    the complete range of possible ratings.
-
-    kappa(X, min_rating, max_rating), where min_rating
-    is the minimum possible rating, and max_rating is the maximum possible
-    rating
-    """
-    assert(len(rater_a) == len(rater_b))
-    if min_rating is None:
-        min_rating = min(rater_a + rater_b)
-    if max_rating is None:
-        max_rating = max(rater_a + rater_b)
-    conf_mat = confusion_matrix(rater_a, rater_b,
-                                min_rating, max_rating)
-    num_ratings = len(conf_mat)
-    num_scored_items = float(len(rater_a))
-
-    hist_rater_a = histogram(rater_a, min_rating, max_rating)
-    hist_rater_b = histogram(rater_b, min_rating, max_rating)
-
-    numerator = 0.0
-    denominator = 0.0
-
-    for i in range(num_ratings):
-        for j in range(num_ratings):
-            expected_count = (hist_rater_a[i] * hist_rater_b[j]
-                              / num_scored_items)
-            if i == j:
-                d = 0.0
-            else:
-                d = 1.0
-            numerator += d * conf_mat[i][j] / num_scored_items
-            denominator += d * expected_count / num_scored_items
-
-    return 1.0 - numerator / denominator
-
-
-def mean_quadratic_weighted_kappa(kappas, weights=None):
-    """
-    Calculates the mean of the quadratic
-    weighted kappas after applying Fisher's r-to-z transform, which is
-    approximately a variance-stabilizing transformation.  This
-    transformation is undefined if one of the kappas is 1.0, so all kappa
-    values are capped in the range (-0.999, 0.999).  The reverse
-    transformation is then applied before returning the result.
-
-    mean_quadratic_weighted_kappa(kappas), where kappas is a vector of
-    kappa values
-
-    mean_quadratic_weighted_kappa(kappas, weights), where weights is a vector
-    of weights that is the same size as kappas.  Weights are applied in the
-    z-space
-    """
-    kappas = np.array(kappas, dtype=float)
-    if weights is None:
-        weights = np.ones(np.shape(kappas))
-    else:
-        weights = weights / np.mean(weights)
-
-    # ensure that kappas are in the range [-.999, .999]
-    kappas = np.array([min(x, .999) for x in kappas])
-    kappas = np.array([max(x, -.999) for x in kappas])
-
-    z = 0.5 * np.log((1 + kappas) / (1 - kappas)) * weights
-    z = np.mean(z)
-    return (np.exp(2 * z) - 1) / (np.exp(2 * z) + 1)
-
-
-def weighted_mean_quadratic_weighted_kappa(solution, submission):
-    predicted_score = submission[submission.columns[-1]].copy()
-    predicted_score.name = "predicted_score"
-    if predicted_score.index[0] == 0:
-        predicted_score = predicted_score[:len(solution)]
-        predicted_score.index = solution.index
-    combined = solution.join(predicted_score, how="left")
-    groups = combined.groupby(by="essay_set")
-    kappas = [quadratic_weighted_kappa(group[1]["essay_score"], group[1]["predicted_score"]) for group in groups]
-    weights = [group[1]["essay_weight"].irow(0) for group in groups]
-    return mean_quadratic_weighted_kappa(kappas, weights=weights)
