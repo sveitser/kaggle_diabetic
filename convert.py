1a9d47da6cbb38cc7deb8f9add675bb7b9c668cc
Mathis Antony <sveitser@gmail.com>
2015-08-03 16:18:54 +0800

    remove unused function and CLI argument

--- convert.py
+++ convert.py
@@ -1,15 +1,7 @@
-"""Resize and crop images to square, save as tiff.
-
-Assumes the training data is in ./data/train
-"""
+"""Resize and crop images to square, save as tiff."""
 from __future__ import division, print_function
 import os
 from multiprocessing.pool import Pool
-
-from skimage import exposure
-from skimage.exposure import equalize_adapthist
-from skimage.filters import rank
-from skimage.morphology import disk
 
 import click
 import numpy as np
@@ -71,36 +63,6 @@ def convert_square(fname, crop_size):
     return resized
 
 
-def convert_micro(fname, crop_size):
-    img = Image.open(fname)
-
-    blurred = img.filter(ImageFilter.BLUR)
-    ba = np.array(blurred)
-    _, w, _ = ba.shape
-
-    left_max = ba[:, : w / 64, :].max(axis=(0, 1)).astype(int)
-    right_max = ba[:, - w / 64:, :].max(axis=(0, 1)).astype(int)
-    max_bg = np.maximum(left_max, right_max)
-
-    foreground = (ba > max_bg + 10).astype(np.uint8)
-    bbox = Image.fromarray(foreground).getbbox()
-
-    cropped = img.crop(bbox).resize([crop_size, crop_size], 
-                                    resample=Image.LANCZOS)
-
-    green = np.array(cropped)[:, :, 1]
-    green = equalize_adapthist(np.array(green, dtype=np.float32) / 255)
-
-    med = rank.median(green, disk(20))
-
-    result = med.astype(np.float32) - 255 * green.astype(np.float32)
-
-    result -= np.min(result)
-    result *= 255 / np.max(result)
-
-    return Image.fromarray(result.astype(np.uint8))
-
-
 def get_convert_fname(fname, extension, directory, convert_directory):
     return fname.replace('jpeg', extension).replace(directory, 
                                                     convert_directory)
@@ -125,8 +87,7 @@ def save(img, fname):
 @click.option('--test', is_flag=True, default=False)
 @click.option('--crop_size', default=256)
 @click.option('--extension', default='tiff')
-@click.option('--micro', is_flag=True, default=False)
-def main(directory, convert_directory, test, crop_size, extension, micro):
+def main(directory, convert_directory, test, crop_size, extension):
 
     try:
         os.mkdir(convert_directory)
@@ -138,13 +99,12 @@ def main(directory, convert_directory, test, crop_size, extension, micro):
     filenames = sorted(filenames)
 
     if test:
-        fun = convert_micro if micro else convert
         names = data.get_names(filenames)
         y = data.get_labels(names)
         for f, level in zip(filenames, y):
             if level == 1:
                 try:
-                    img = fun(f, crop_size)
+                    img = convert(f, crop_size)
                     img.show()
                     Image.open(f).show()
                     real_raw_input = vars(__builtins__).get('raw_input',input)
@@ -156,17 +116,15 @@ def main(directory, convert_directory, test, crop_size, extension, micro):
           "".format(directory, convert_directory))
 
     n = len(filenames)
-    # process in batches, sometimes weird things happen with Pool
+    # process in batches, sometimes weird things happen with Pool on my machine
     batchsize = 500
     batches = n // batchsize + 1
     pool = Pool(N_PROC)
 
     args = []
 
-    fun = convert_micro if micro else convert
-
     for f in filenames:
-        args.append((fun, (directory, convert_directory, f, crop_size, 
+        args.append((convert, (directory, convert_directory, f, crop_size, 
                            extension)))
 
     for i in range(batches):
